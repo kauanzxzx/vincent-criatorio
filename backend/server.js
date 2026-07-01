44a1ac1a5e173d8c272c624711152aa95e1560a6
kauanzxzx <kauanclaudino980@gmail.com>
2026-07-01 19:16:28 -0300

    Debug put cruzamentos

--- backend/server.js
+++ backend/server.js
@@ -327,6 +327,8 @@ app.post("/cruzamentos", (req,res)=>{
 
 app.put("/cruzamentos/:id", (req,res)=>{
 
+    console.log("BODY CRUZAMENTO:", req.body);
+
     db.query(
         "UPDATE cruzamentos SET ? WHERE id_cruzamento=?",
         [
@@ -336,6 +338,9 @@ app.put("/cruzamentos/:id", (req,res)=>{
         (erro)=>{
 
             if(erro){
+
+                console.error("ERRO PUT CRUZAMENTOS:", erro);
+
                 return res.status(500).json(erro);
             }
 
